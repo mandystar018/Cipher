6e99161ccb7c939dc2fbf869c856f01d63b30df8
Amanda Otero <mandystar018@gmail.com>
2020-08-10 21:37:31 -0700

    This commit will add script.js and functions

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const sentence = prompt("Enter a Sentence:");
+console.log (sentence);
+
+function firstLetter (sentence){
+  return (sentence.charAt(0));
+}
+
+function lastLetter (sentence){
+  return (sentence.charAt(5));
+}
+
